27d4ae955885d2a30d69102693a59ebc2bc52b12
elnicomapero <75832950+elnicomapero@users.noreply.github.com>
2021-09-25 19:40:48 +0800

    Experiment with CRA

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <h1>Hello there, elnicomapero's World!</h1>
+    );
+}
+ 
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
